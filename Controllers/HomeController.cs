3ac1b4fb0885e952a5fa1342835164759571cf3c
admin <hluthala@yahoo.fr>
2022-08-31 17:48:32 +0100

    initalisation

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mysolution.Models;
+
+namespace Mysolution.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var newperso=new infopersonnel{
+            id=1,
+            nom="test",
+            Age=10
+            };
+            return View(newperso);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/infopersonnel.cs
@@ -0,0 +1,9 @@
+namespace Mysolution.Models
+{
+    public class infopersonnel
+    {
+        public int id { get; set; }
+        public string nom { get; set; }
+        public int Age { get; set; }
+    }
+}
